5c09a313f167bb7336531f5104b2cd3f9a02d24a
dj <dj@ime.usp.br>
2022-02-04 09:20:04 -0300

    Primeira tela com botão que altera texto a partir do texto editável.

--- app/src/main/java/com/oceanbrasil/ocean_android_retroglide_fev_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_retroglide_fev_2022/MainActivity.kt
@@ -2,10 +2,28 @@ package com.oceanbrasil.ocean_android_retroglide_fev_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val texto = findViewById<TextView>(R.id.texto)
+        val textoEditavel = findViewById<EditText>(R.id.textoEditavel)
+        val botaoAtualizarTexto = findViewById<Button>(R.id.botaoAtualizarTexto)
+
+        botaoAtualizarTexto.setOnClickListener {
+            val novoTexto = textoEditavel.text.toString()
+
+            if (novoTexto.isNotBlank()) {
+                texto.text = novoTexto
+            } else {
+                textoEditavel.error = "Digite um texto, por gentileza!"
+            }
+        }
+
     }
 }
