4476fd2907058beaf46e7bba8a890f75e1fa01de
dj <dj@ime.usp.br>
2022-02-04 11:58:09 -0300

    Quarta aula segunda parte: Glide funcionando

--- app/src/main/java/com/oceanbrasil/ocean_android_retroglide_fev_2022/GithubActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_retroglide_fev_2022/GithubActivity.kt
@@ -2,7 +2,10 @@ package com.oceanbrasil.ocean_android_retroglide_fev_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,9 +17,11 @@ class GithubActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_github)
 
-        // Retrofit
-        val textoDoGithub = findViewById<TextView>(R.id.textoGithub)
+        val nomeDoPrimeiroRepositorio = findViewById<TextView>(R.id.nomeDoRepositorio)
+        val donoDoRepositorio = findViewById<TextView>(R.id.donoDoRepositorio)
+        val avatar = findViewById<View>(R.id.avatar) as ImageView
 
+        // Retrofit
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -37,12 +42,18 @@ class GithubActivity : AppCompatActivity() {
                     val body = response.body()
 
                     body?.get(0).let {
-                        textoDoGithub.setText(it?.name)
+                        nomeDoPrimeiroRepositorio.setText(it?.name)
+                        donoDoRepositorio.setText(it?.owner?.login)
+                        // Glide
+                        Glide
+                            .with(this@GithubActivity)
+                            .load(it?.owner?.avatar_url)
+                            .into(avatar)
                     }
                 }
 
                 override fun onFailure(call: Call<List<Repo?>?>, t: Throwable) {
-                    textoDoGithub.setText("Ocorreu algum erro!")
+                    nomeDoPrimeiroRepositorio.setText("Ocorreu algum erro!")
                 }
 
             }
